28488cc56c0c99b33e5f820a9faccb18ac703d0e
dev-edson <edsonpereira0507@gmail.com>
2022-04-15 08:58:08 -0400

    Criação dinamica de pasta

--- backend/src/utilizar-sqlite.js
+++ backend/src/utilizar-sqlite.js
@@ -6,6 +6,13 @@ function tratarErro(err) {
     }
 }
 
+// Verificação e criação de pasta (Se a pasta não existir vai cria-la)
+const fs = require('fs');
+const PASTA_DATABASE = 'backend/src/database';
+
+if (!fs.existsSync(PASTA_DATABASE)) {
+    fs.mkdirSync(PASTA_DATABASE)
+}
 
 //Cria o banco de dados "database.db" na pasta src/database
 let bancoDeDados = new sqlite.Database('backend/src/database/device_manager.db', tratarErro)
@@ -26,6 +33,41 @@ bancoDeDados.run(`
 //    tratarErro
 //)
 
+//bancoDeDados.run(
+//    `INSERT INTO devices 
+//    (category_id, device_color, device_partnumber) 
+//    VALUES 
+//    (?, ?, ?)
+//    `, 2, 'Vermelha', 452020,
+//    tratarErro
+//)
+
+bancoDeDados.run(
+    `INSERT INTO devices 
+    (category_id, device_color, device_partnumber) 
+    VALUES 
+    ($category_id, $device_color, $device_partnumber)`,
+    {
+        $category_id: '3',
+        $device_color: 'Azul',
+        $device_partnumber: 452021
+    },
+    tratarErro
+)
+
+// Preparação de script para utilizar mais de uma vez
+let stmt = bancoDeDados.prepare(
+    "INSERT INTO devices (category_id, device_color, device_partnumber) VALUES (?, ?, ?)",
+    tratarErro
+)
+
+for (let index = 0; index < 10; index++) {
+    stmt.run(4, 'Azul', 222020)
+}
+
+stmt.finalize()
+
+
 //bancoDeDados.all(
 bancoDeDados.all(
     "SELECT * FROM devices",
@@ -34,14 +76,31 @@ bancoDeDados.all(
     }
 )
 
+bancoDeDados.each(
+    "SELECT * FROM devices",
+    function (err, row) {
+        console.log('ITEM: ', row)
+    }
+)
+
+//Primeiro item
+bancoDeDados.get(
+    "SELECT * FROM devices",
+    function (err, row) {
+        console.log('SOMENTE UM: ', row)
+    }
+)
+
 // Atualização de valores na tabela
-//bancoDeDados.run(
-//    "UPDATE devices SET device_color = 'Branca' WHERE device_id = 2",
-//    tratarErro
-//)
+bancoDeDados.run(
+    "UPDATE devices SET device_color = 'Branca' WHERE device_id = 2",
+    tratarErro
+)
 
 // Remoção de valores na tabela
-//bancoDeDados.run(
-//    "DELETE FROM devices WHERE device_id = 2",
-//    tratarErro
-//)
+bancoDeDados.run(
+    "DELETE FROM devices WHERE device_id = 2",
+    tratarErro
+)
+
+bancoDeDados.close()
