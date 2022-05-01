f8349b4181b331862c339bbdb68f1e2cb178ec6e
dev-edson <edsonpereira0507@gmail.com>
2022-05-01 10:11:36 -0400

    Aplicação dos endpoint.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const express = require('express')
 
 //Iniciar express
@@ -8,7 +9,7 @@ app.use(express.json())
 
 //http://localhost:3333/BuscarDevices
 
-const devices = [
+let devices = [
     { id: 1, partnumber: 1234560, cor: 'Amarela' },
     { id: 2, partnumber: 1234561, cor: 'Azul' },
     { id: 3, partnumber: 1234562, cor: 'Vermelha' },
@@ -16,8 +17,26 @@ const devices = [
 
 
 //Criar um endpoint
-app.get('/Devices', (request, response) => {
-    return response.json({ devices })
+app.get('/devices', (request, response) => {
+    const parametros = request.query
+
+    let devicesFiltrados = devices
+
+    if (parametros) {
+        devicesFiltrados = devices.filter(device => {
+            if (parametros.cor && parametros.partnumber) {
+                return device.cor === parametros.cor && device.partnumber === parseInt(parametros.partnumber)
+            } else if (parametros.cor) {
+                return device.cor === parametros.cor
+            } else if (parametros.partnumber) {
+                return device.partnumber === parseInt(parametros.partnumber)
+            } else {
+                return device
+            }
+        })
+    }
+
+    return response.json({ devices: devicesFiltrados })
 })
 
 //Criar um endpoint com paramnetro
@@ -30,15 +49,39 @@ app.get('/Devices/:id', (request, response) => {
 })
 
 //Adicionar um carro
-app.post('/Devices', (request, response) => {
+app.post('/devices', (request, response) => {
     const device = request.body
 
-    device.id = device.length + 1
+    device.id = devices.length + 1
+    devices.push(device)
 
-    device.push(device)
-
-    return response.json({ mensagem: 'Carro cadastrado com sucesso!' })
+    return response.json({ mensagem: 'Device cadatrado com sucesso!' })
 })
+
+//Atualizar um device
+app.put('/devices/:id', (request, response) => {
+    const deviceId = request.params.id
+    const novoDevice = request.body
+
+    devices = devices.map(device => {
+        if (device.id === parseInt(deviceId)) {
+            return Object.assign(device, novoDevice)
+        } else {
+            return device
+        }
+    })
+    return response.json({ mensagem: 'Device atualizado com sucesso!' })
+})
+
+//Deletar um device
+app.delete('/devices/:id', (request, response) => {
+    const deviceId = request.params.id
+
+    devices = devices.filter(device => device.id !== parseInt(deviceId))
+
+    return response.json({ mensagem: 'Device deletado com sucesso!' })
+})
+
 
 //Iniciar servidor
 app.listen(3333, () => {
